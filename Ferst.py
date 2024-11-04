c09edbaabb464df14b371d4c426a1caadf992192
zhenia.petrov01 <zhenia.petrov01@yandex.ru>
2024-11-04 14:44:23 +0300

    first commit

---
+++ Ferst.py
@@ -0,0 +1,12 @@
+a = input()
+simvol = []
+p1 = 0
+for i in range(len(a)):
+    p2 = 0
+    for j in range(len(a)):
+        if a[i] == a[j]:
+            p2 += 1
+    if p1 < p2:
+        p1 = p2
+        simvol = a[i]
+print(simvol)
