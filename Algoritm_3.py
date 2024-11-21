15f7e94f0d766f6d92d12ec377f2ca86a1c58387
zhenia.petrov01 <zhenia.petrov01@yandex.ru>
2024-11-21 20:28:41 +0300

    new commit

--- Algoritm_3.py
+++ Algoritm_3.py
@@ -1,13 +1,14 @@
-s = input('Введите текст:')
+s = input('Введите текст: ')
 ans = ''
 anscnt = 0
 dct = {}
 for now in s:
     if now not in dct:
         dct[now] = 0
-    dct[now] += 1
+    dct[now] = dct[now] + 1
 for key in dct:
-    if  dct[key] < anscnt:
+    if  dct[key] > anscnt:
         anscnt = dct[key]
         ans = key
 print(ans)
+#Код пока не работает
